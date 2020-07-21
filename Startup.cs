b63ee002b937d3f5cd02b7063c64fb3bf1e964d1
Ryan Gordon <ryanmg@bgsu.edu>
2020-07-21 02:16:59 -0400

    Added Cors Service so that SPA is allowed to get data from API

--- Startup.cs
+++ Startup.cs
@@ -29,6 +29,7 @@ namespace FriendApp.API
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +43,8 @@ namespace FriendApp.API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseAuthorization();
 
