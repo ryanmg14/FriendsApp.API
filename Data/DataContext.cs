2e7ee6e0ca2a4dd403ced8f97b457aef936fbe10
Ryan Gordon <ryanmg@bgsu.edu>
2020-07-23 22:27:38 -0400

    Added User Model/Table to database and implementing Repository pattern
    Beginning repository pattern by creating IAuthRespository interface

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,5 +1,6 @@
 using FriendApp.API.Controllers.Models;
 using Microsoft.EntityFrameworkCore;
+using FriendApp.API.Models;
 
 namespace FriendApp.API.Data
 {
@@ -7,5 +8,7 @@ namespace FriendApp.API.Data
     {
         public DataContext(DbContextOptions<DataContext> options) : base (options){ }
         public DbSet<Value> Values { get; set; }
+
+        public DbSet<User> Users { get; set; }
     }
 }
---
+++ Data/IAuthRepository.cs
@@ -0,0 +1,11 @@
+using System.Threading.Tasks;
+using FriendApp.API.Models;
+
+namespace FriendApp.API.Data
+{
+    public interface IAuthRepository
+    {
+         Task<User> Register(User user, string password);
+         Task<User> Login(string username, string password);
+    }
+}
