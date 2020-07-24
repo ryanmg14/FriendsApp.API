e86de06033ba8ab1d670044556b5301f11f0bb0c
Ryan Gordon <ryanmg@bgsu.edu>
2020-07-24 00:45:37 -0400

    Implemented AuthRepository from interface

--- Data/IAuthRepository.cs
+++ Data/IAuthRepository.cs
@@ -7,5 +7,6 @@ namespace FriendApp.API.Data
     {
          Task<User> Register(User user, string password);
          Task<User> Login(string username, string password);
+         Task<bool> UserExists(string username);
     }
 }
