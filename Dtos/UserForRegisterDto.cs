52870fd85ba03fd8d17bec7ef7221661d81506a5
Ryan Gordon <ryanmg@bgsu.edu>
2020-07-27 22:58:15 -0400

    Implemented Login method for AuthController
    The authetication is done using tokens.
    Token key was added to appsetting.json.
    UserForLoginDto was created for the data transfer
    2 packages were required to be added to FriendApp.API.csproj

--- Dtos/UserForRegisterDto.cs
+++ Dtos/UserForRegisterDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FriendApp.API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username { get; set; }
+
+        [Required]
+        [StringLength(16, MinimumLength = 8, ErrorMessage = "Password must be 8 - 16 characters")]
         public string Password { get; set; }
 
     }
